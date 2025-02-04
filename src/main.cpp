fb04821a8ef7830b031cb20425abdb255eac55cc
kbl <kbl@superfood>
2025-02-04 20:28:29 +0100

    First UDP-client code..

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include "sender.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const std::string ip = "127.0.0.1";
+    const uint16_t port = 8080;
+
+    UDPSender sender(ip, port);
+
+    std::string message = "Hello, UDP!";
+    if (sender.sendMessage(message)) {
+        std::cout << "Message sent successfully!\n";
+    } else {
+        std::cerr << "Failed to send message.\n";
+    }
+
     return 0;
 }
