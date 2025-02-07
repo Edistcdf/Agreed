c5407a5c359a6fd79dbc8746c2d25bc926865492
dEV <deeperseeker@100X.altman>
2025-02-07 19:56:00 +0100

    Textbox..can press enter... Updated README for idiots who run shitland

--- README.md
+++ README.md
@@ -19,3 +19,18 @@ podman run -it --rm \
        localhost/agreed-dev
 ```
 possibly `sudo setenforce 0`
+
+## If using Wayland
+``` bash
+podman run \
+	--userns=keep-id \
+	-e QT_QPA_PLATFORM=wayland \
+	-e GDK_BACKEND=wayland \
+	--device /dev/dri \
+	--security-opt label=typer:container_runtime_t \
+	-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+	-v /run/user/$(id -u)/$WAYLAND_DISPLAY:/run/user/$(id -u)/$WAYLAND_DISPLAY \
+	-e XDG_RUNTIME_DIR=/run/user/$(id -u) \
+	-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+	--privileged --rm -it -e DISPLAY=$DISPLAY -v ${HOME}/.profile:${HOME}/.profile -v ${HOME}/.bashrc:${HOME}/.bashrc -v <path-to>/Agreed/:/workspace localhost/dev
+```
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,10 +1,43 @@
-#include <QApplication>
 #include <QMainWindow>
-#include "mainwindow.h"
+#include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QDebug>
+
+class ChatWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    ChatWindow(QWidget *parent = nullptr) : QWidget(parent) {
+        textInput = new QLineEdit(this);
+        textInput->setPlaceholderText("write...");
+        QVBoxLayout *layout = new QVBoxLayout();
+        layout->addWidget(textInput);
+        setLayout(layout);
+
+        connect(textInput, &QLineEdit::returnPressed, this, &ChatWindow::onEnterPressed);
+    }
+
+private slots:
+    void onEnterPressed() {
+        QString inputText = textInput->text();
+        if (!inputText.isEmpty()) {
+            qDebug() << "text: " << inputText;
+            textInput->clear();
+        }
+    }
+
+private:
+    QLineEdit *textInput;
+};
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    MainWindow window;
+    ChatWindow window;
+    window.setWindowTitle("CryptoChat");
     window.show();
     return app.exec();
 }
+
+#include "gui.moc"
